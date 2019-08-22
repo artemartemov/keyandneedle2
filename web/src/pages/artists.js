f52e370f693bbd0a44de5cbf6b9bfa0f55b6357e
Artem Artemov <aartemov@hearst.com>
2019-08-21 22:00:00 -0600

    removed query

--- web/src/pages/artists.js
+++ web/src/pages/artists.js
@@ -5,26 +5,6 @@ import styled from 'styled-components';
 
 export const query = graphql`
   query ArtistPageQuery {
-    site: sanitySiteSettings(_id: { eq: "siteSettings" }) {
-      title
-      description
-      keywords
-    }
-
-    homepage: sanityIndexPage(_id: { eq: "indexPage" }) {
-      headlineText
-      _rawSubhead
-      buttonText
-      bgImages {
-        asset {
-          fluid(maxWidth: 1800) {
-            ...GatsbySanityImageFluid
-          }
-          url
-          _id
-        }
-      }
-    }
   }
 `;
 

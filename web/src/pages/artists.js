bf000fb55edea9e44af7ab877e20593b7cb7bc01
Artem Artemov <aartemov@hearst.com>
2019-08-21 22:04:24 -0600

    removed query fuly

--- web/src/pages/artists.js
+++ web/src/pages/artists.js
@@ -3,11 +3,6 @@ import { PropTypes } from 'prop-types';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
 
-export const query = graphql`
-  query ArtistPageQuery {
-  }
-`;
-
 const ArtistListingPage = ({ data, errors }) => false;
 
 export default ArtistListingPage;

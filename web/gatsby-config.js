3883e682d5e1967204f04bd8ab7840c1fce83099
Artem Artemov <aartemov@hearst.com>
2020-01-14 18:58:20 -0700

    updated frontend to pull in cms data

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,7 +9,19 @@ const clientConfig = require("./client-config");
 
 const isProd = process.env.NODE_ENV === "production";
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://www.keyandneedle.com",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === "production";
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
+  siteMetadata: {
+    siteUrl
+  },
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
@@ -37,6 +49,39 @@ module.exports = {
         rule: {
           include: /icons/
         }
+      }
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        query: `{
+          site: sanitySiteSettings(_id: { eq: "siteSettings" }) {
+            siteUrl
+          }
+        }`,
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*" }]
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null
+          }
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-120158888-1`
       }
     }
   ]
--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,69 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+import SchemaOrg from 'components/SchemaOrg';
+// import { imageUrlFor } from '../lib/image-url';
+// import { buildImageObj } from '../lib/helpers';
 
-function SEO({ metaDescription, lang, meta, keywords, title, title: siteTitle }) {
+function SEO({ description, lang, meta, keywords, title }) {
   return (
-    <Helmet
-      htmlAttributes={{ lang }}
-      title={title}
-      titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
-      meta={[
-        {
-          name: 'description',
-          content: metaDescription,
-        },
-        {
-          property: 'og:title',
-          content: title,
-        },
-        {
-          property: 'og:description',
-          content: metaDescription,
-        },
-        {
-          property: 'og:type',
-          content: 'website',
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary',
-        },
-        {
-          name: 'twitter:creator',
-          content: title,
-        },
-        {
-          name: 'twitter:title',
-          content: title,
-        },
-        {
-          name: 'twitter:description',
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords && keywords.length > 0
-            ? {
-                name: 'keywords',
-                content: keywords.join(', '),
-              }
-            : []
-        )
-        .concat(meta)}
+    <StaticQuery
+      query={detailsQuery}
+      render={data => {
+        const metaDescription = description || (data.site && data.site.description) || '';
+        const siteTitle = (data.site && data.site.title) || '';
+        const siteAuthor = (data.site && data.site.author) || '';
+        // const metaImage =
+        //   image && image.asset
+        //     ? imageUrlFor(buildImageObj(image))
+        //         .width(1200)
+        //         .url()
+        //     : '';
+
+        return (
+          <>
+            <Helmet
+              htmlAttributes={{ lang }}
+              title={siteTitle}
+              titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
+              meta={[
+                {
+                  name: 'description',
+                  content: metaDescription,
+                },
+                {
+                  property: 'og:title',
+                  content: siteTitle,
+                },
+                {
+                  property: 'og:description',
+                  content: metaDescription,
+                },
+                {
+                  property: 'og:type',
+                  content: 'website',
+                },
+                // {
+                //   property: 'og:image',
+                //   content: metaImage,
+                // },
+                {
+                  name: 'twitter:card',
+                  content: 'summary',
+                },
+                {
+                  name: 'twitter:creator',
+                  content: siteAuthor,
+                },
+                {
+                  name: 'twitter:title',
+                  content: siteTitle,
+                },
+                {
+                  name: 'twitter:description',
+                  content: metaDescription,
+                },
+              ]
+                .concat(
+                  keywords && keywords.length > 0
+                    ? {
+                        name: 'keywords',
+                        content: keywords.join(', '),
+                      }
+                    : []
+                )
+                .concat(meta)}
+            />
+            <SchemaOrg
+              url={data.site.siteUrl}
+              title={siteTitle}
+              description={metaDescription}
+              canonicalUrl={data.site.siteUrl}
+              author={data.site.author}
+              organization="Key+Needle"
+              defaultTitle={siteTitle}
+              facebookUrl={data.site.facebookUrl}
+              twitterUrl={data.site.twitterUrl}
+              soundcloudUrl={data.site.soundcloudUrl}
+            />
+          </>
+        );
+      }}
     />
   );
 }
 
 SEO.defaultProps = {
   lang: 'en',
-  metaDescription: '',
   meta: [],
   keywords: [],
+  description: '',
 };
 
 SEO.propTypes = {
-  metaDescription: PropTypes.string,
+  description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
@@ -71,3 +110,18 @@ SEO.propTypes = {
 };
 
 export default SEO;
+
+const detailsQuery = graphql`
+  query DefaultSEOQuery {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      description
+      keywords
+      siteUrl
+      author
+      facebookUrl
+      twitterUrl
+      soundcloudUrl
+    }
+  }
+`;
